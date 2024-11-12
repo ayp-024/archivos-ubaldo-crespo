0d0049c76001b6563956fdda3a90f8c105324ee5
Ubaldo <ubaldo-crespo@iteso.mx>
2024-11-12 10:00:02 -0600

    Dodcstring agregada a la función para promedios

--- promedios.py
+++ promedios.py
@@ -2,7 +2,15 @@ from math import ceil
 
 
 def average_grades_in_file(input_file: str, output_file: str):
-    "docstring"
+    """
+    Dado un archivo de texto con nombres y calificaciones de alumnos de un
+    curso, con el siguiente formato:
+        Peter Parker 100 95 98 92
+    Procesarlo y generar otro archivo de texto con el formato:
+        PARKER, Peter: 96.3
+
+    Todos los alumnos tienen un solo nombre y apellido.
+    """
     with open(input_file, 'r', encoding="utf8") as in_f:
         with open(output_file, 'w', encoding="utf8") as out_f:
             for line in in_f:
